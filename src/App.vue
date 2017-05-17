3c50d20b0a6d4b52deb202a125b2f00a57c9a050
jianglin <jianglin@jianglindeMacBook-Pro.local>
2017-05-17 12:17:28 +0800

    aa

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app" >
+    <div class="styleEditor">
+      <pre>
+       {{ code }}        
+      </pre>
+    </div>
+    <div v-html="styleCode">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      // 输出到页面上显示的  
+      code: ``,
+      // 最终代码
+      finalCode: `
+        body {
+          background: red;
+        }
+      `,
+      // 样式代码
+      styleCode: ''
+    }
+  },
+  created: function () {
+    var n = 0
+    setInterval(()=> {
+      this.code= this.finalCode.substring(0, n)
+      n += 1
+    }, 100)
+  },
+  computed: {
+    styleCode: function () {
+      return '<style>' + this.code + '</style>'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
